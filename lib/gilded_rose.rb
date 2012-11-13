867309a4ef339c01faaf1e50311ed86183ca2ff4
adammartin <ebondark.od@gmail.com>
2012-11-12 21:41:09 -0600

    Fixing incorrect touching of the @items attribute don't want a goblin to one shot me

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,18 +4,22 @@ require 'inventory'
 
 class GildedRose
 
-  attr_accessor :items
+  @items = []
   @inventory
+
+  def items
+    @items
+  end
 
   def initialize
     @items = []
-    @items << StandardItem.new(Item.new("+5 Dexterity Vest", 10, 20))
-    @items << StandardItem.new(Item.new("Aged Brie", 2, 0))
-    @items << StandardItem.new(Item.new("Elixir of the Mongoose", 5, 7))
-    @items << StandardItem.new(Item.new("Sulfuras, Hand of Ragnaros", 0, 80))
-    @items << StandardItem.new(Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20))
-    @items << StandardItem.new(Item.new("Conjured Mana Cake", 3, 6))
-    @inventory = Inventory.new(@items)
+    @items << Item.new("+5 Dexterity Vest", 10, 20)
+    @items << Item.new("Aged Brie", 2, 0)
+    @items << Item.new("Elixir of the Mongoose", 5, 7)
+    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
+    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    @items << Item.new("Conjured Mana Cake", 3, 6)
+    @inventory = Inventory.new(@items.collect{|an_item| StandardItem.new(an_item)})
   end
 
   def age_by_a_day
