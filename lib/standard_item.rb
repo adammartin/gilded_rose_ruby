b827eaf030f6afe30f7053fe306c02b19a7da441
adammartin <ebondark.od@gmail.com>
2012-11-13 22:19:01 -0600

    Removing dead code

--- lib/standard_item.rb
+++ lib/standard_item.rb
@@ -2,49 +2,13 @@ require "base_item"
 
 class StandardItem < BaseItem
   def age_by_a_day
-      if (@my_item.name != "Aged Brie" && @my_item.name != "Backstage passes to a TAFKAL80ETC concert")
-        if (@my_item.quality > 0)
-          if (@my_item.name != "Sulfuras, Hand of Ragnaros")
-            @my_item.quality = @my_item.quality - 1
-          end
-        end
-      else
-        if (@my_item.quality < 50)
-          @my_item.quality = @my_item.quality + 1
-          if (@my_item.name == "Backstage passes to a TAFKAL80ETC concert")
-            if (@my_item.sell_in < 11)
-              if (@my_item.quality < 50)
-                @my_item.quality = @my_item.quality + 1
-              end
-            end
-            if (@my_item.sell_in < 6)
-              if (@my_item.quality < 50)
-                @my_item.quality = @my_item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if (@my_item.name != "Sulfuras, Hand of Ragnaros")
-        @my_item.sell_in = @my_item.sell_in - 1;
-      end
-      if (@my_item.sell_in < 0)
-        if (@my_item.name != "Aged Brie")
-          if (@my_item.name != "Backstage passes to a TAFKAL80ETC concert")
-            if (@my_item.quality > 0)
-              if (@my_item.name != "Sulfuras, Hand of Ragnaros")
-                @my_item.quality = @my_item.quality - 1
-              end
-            end
-          else
-            @my_item.quality = @my_item.quality - @my_item.quality
-          end
-        else
-          if (@my_item.quality < 50)
-            @my_item.quality = @my_item.quality + 1
-          end
-        end
-      end
-    end
+    @my_item.quality = (@my_item.quality > 0) ? @my_item.quality - decay_rate : 0
+    @my_item.sell_in -= 1;
+  end
 
+  private
+
+  def decay_rate
+    @my_item.sell_in > 0 ? 1 : 2
+  end
 end
